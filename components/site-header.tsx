56afe5e7798d43951bd0ff051473e0ab310e4d2b
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2023-06-20 15:50:55 -0300

    add: hero on main page

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,15 +3,18 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
+
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-        <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
+    <header className="fixed z-40 w-full ">
+      <div className="flex h-16 items-center sm:justify-between sm:space-x-0">
+        <div className="flex absolute left-8 gap-6 md:gap-10">
+          <Link href="/" className="flex items-center space-x-2">
+            <span className="inline-block font-[300] text-[#FFCD00]">V-REJECTS</span>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4 pr-8">
           <nav className="flex items-center space-x-1">
             <Link
               href={siteConfig.links.github}
@@ -24,26 +27,10 @@ export function SiteHeader() {
                   variant: "ghost",
                 })}
               >
-                <Icons.gitHub className="h-5 w-5" />
+                <Icons.gitHub className="h-5 w-5 text-[#FFCD00]" />
                 <span className="sr-only">GitHub</span>
               </div>
             </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
-            <ThemeToggle />
           </nav>
         </div>
       </div>
