f9c3ccc60798eec92acc772ca38fa27bf25e9901
Leo <leonardomjq@gmail.com>
2023-06-21 11:31:19 +0100

    added new routes

---
+++ app/browse/page.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import SearchBar from "@/components/search-bar"
+
+export default function page() {
+  return (
+    <div>
+      <div className="h-screen">
+        <div className="flex justify-center pt-8">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,12 @@
 import Link from "next/link"
 
 import SiteHero from "@/components/site-hero"
-import SearchBar from "@/components/search-bar"
 
 export default function IndexPage() {
   return (
     <section className="overflow-hidden">
       <SiteHero />
-      <div className="h-screen">
-        <div className="flex justify-center pt-8">
-          <SearchBar />
-        </div>
-      </div>
-      <SiteHero />
-
     </section>
   )
 }
+  
