2faa0429fbdfdcfd607c05341ece3c822dfcc55a
Augusto Goulart <93224568+GoulartAugusto@users.noreply.github.com>
2023-06-26 18:32:01 -0300

    creating the functions n' states for event data

--- app/postevent/page.tsx
+++ app/postevent/page.tsx
@@ -1,9 +1,68 @@
+"use client"
+
 import React from 'react'
 import { DatePicker } from '@/components/date-picker'
 import { TypeInput } from '@/components/type-input'
 import { PlatformInput } from '@/components/platform-input'
+import { useState, useEffect } from 'react'
 
 export default function page() {
+
+  // the functions and states are used for uploading the data for the database
+  // a node app has to be configurated to all this stuff work
+
+  const [thumbnail, setThumbnail] = useState("")
+  const [title, setTitle] = useState('')
+  const [eventDate, setEventDate] = useState('')
+
+  const [allData, setAllData] = useState([])
+
+  function convertToBase64(e) {
+    console.log(e)
+    var reader = new FileReader()
+    reader.readAsDataURL(e.target.files[0])
+    reader.onload = () => {
+      console.log(reader.result) // base64encoded string
+      setThumbnail(reader.result)
+    }
+    reader.onerror = (error) => {
+      console.log("Error: ", error)
+    }
+}
+
+
+
+function uploadImage(e) {
+  e.preventDefault()
+  fetch("http://localhost:5000/upload-image", {
+    method: "POST",
+    crossDomain: true,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      thumbnailBase64: thumbnail, 
+      eventTitle: title,
+      eventTime: eventDate
+    })
+  }).then((res) => res.json()).then((data) => {console.log(data)})
+  window.location.reload()
+}
+
+function getImage() {
+  fetch("http://localhost:5000/get-image", {
+    method: "GET",
+  }).then((res) => res.json()).then((data) => {console.log(data), setAllData(data.data)})
+}
+
+useEffect(() => {
+  getImage()
+}, [])
+
+
+
   return (
     <div className='flex justify-center'>
       <div className=' bg-[#AA00FF] w-[400px] flex h-screen justify-center items-center'>
@@ -14,7 +73,7 @@ export default function page() {
           <span className='text-[18px] mb-4 mt-4'>Choose the event date</span>
           <DatePicker />
           <span className='text-[18px] mb-4 mt-4'>Choose the event title</span>
-          <input type="text" placeholder='Event title' className='h-10' />
+          <input type="text" placeholder='&nbsp; Event title' className='h-10' />
           <span className='text-[18px] mb-4 mt-4'>Choose the event type</span>
           <TypeInput />
           <span className='text-[18px] mb-4 mt-4'>Choose the event Platform</span>
