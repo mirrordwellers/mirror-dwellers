1094a9e15666f306fa6ca3efea423d326004ae69
Leo <leonardomjq@gmail.com>
2023-07-12 14:48:00 +0100

    postevent page changes

--- app/postevent/page.tsx
+++ app/postevent/page.tsx
@@ -1,54 +1,23 @@
 "use client"
 
-import React from 'react'
-import { useState, useEffect } from 'react'
-import Image from 'next/image'
+import React, { useEffect, useState } from "react"
 
-import PlusSign from '@/images/plus-sign.png'
-
-
-import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-import { Grid } from 'lucide-react'
-import { MapPin } from 'lucide-react'
-
-import { Textarea } from "@/components/ui/textarea"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { DatePicker } from "@/components/date-picker"
+import { PlatformInput } from "@/components/platform-input"
+import { TypeInput } from "@/components/type-input"
 
 export default function page() {
-
   // the functions and states are used for uploading the data for the database
   // a node app has to be configurated to all this stuff work
 
   const [thumbnail, setThumbnail] = useState("")
-  const [title, setTitle] = useState('')
-  const [eventDate, setEventDate] = useState('')
-  const [description, setDescription] = useState('')
-  const [eventLink, setEventLink] = useState('')
-
+  const [title, setTitle] = useState("")
+  const [eventDate, setEventDate] = useState("")
 
   const [allData, setAllData] = useState([])
-  const [loading, setLoading] = useState([])
-
 
   function convertToBase64(e) {
     console.log(e)
@@ -61,209 +30,67 @@ export default function page() {
     reader.onerror = (error) => {
       console.log("Error: ", error)
     }
-}
+  }
 
-function uploadEvent(e) {
-  e.preventDefault()
-  fetch("http://localhost:5000/new-event", {
-    method: "POST",
-    crossDomain: true,
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({
-      thumbnail, 
-      eventTitle: title,
-      eventTime: date,
-      eventType: typeOption,
-      platform: platformOption,
-      description: description,
-      eventLink: eventLink,
-
+  function uploadImage(e) {
+    e.preventDefault()
+    fetch("http://localhost:5000/upload-image", {
+      method: "POST",
+      crossDomain: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        thumbnailBase64: thumbnail,
+        eventTitle: title,
+        eventTime: eventDate,
+      }),
     })
-  }).then((res) => res.json()).then((data) => {console.log(data)})
-  window.location.href = "/browse"
-}
-
-function getEvents() {
-  fetch("http://localhost:5000/getAllEvent", {
-    method: "GET",
-  }).then((res) => res.json()).then((data) => {console.log(data), setAllData(data.data)})
-}
-
-useEffect(() => {
-  getEvents()
-}, [])
-
-const [date, setDate] = React.useState<Date>()
-
-const [typeOption, setTypeOption] = useState("+18")
-const EventOptionType = () => {
-   return (
-      <Select value={typeOption} onValueChange={setTypeOption}>
-        <SelectTrigger className="w-[180px] h-[51px] text-[black]">
-          <span><Grid /></span>
-          <SelectValue placeholder="Type of event" />
-        </SelectTrigger>
-        <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Events</SelectLabel>
-              <SelectItem value="Any">Any</SelectItem>
-              <SelectItem value="+18">+18</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-   ) 
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+    window.location.reload()
   }
 
-const [platformOption, setPlatformOption] = useState("Both")
-const EventPlatformOption = () => {
-   return (
-      <Select value={platformOption} onValueChange={setPlatformOption}>
-        <SelectTrigger className="w-[180px] h-[51px] text-[black]">
-          <span><MapPin /></span>
-          <SelectValue placeholder="Type of event" />
-        </SelectTrigger>
-        <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Events</SelectLabel>
-              <SelectItem value="PC">PC</SelectItem>
-              <SelectItem value="Quest">Quest</SelectItem>
-              <SelectItem value="Both">Both</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-   ) 
+  function getImage() {
+    fetch("http://localhost:5000/get-image", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data), setAllData(data.data)
+      })
   }
+
+  useEffect(() => {
+    getImage()
+  }, [])
+
+  // this needs to be a form. I will make this with react hook form + zod (look at shadcn docs)
 
   return (
-    <div className='flex justify-center'>
-      <div className=' bg-[#AA00FF] w-[400px] flex h-full justify-center items-center'>
-        <section className='grid '>
-          <h1 className='text-center font-[500] text-[32px] mb-8'>Create your event</h1>
-          <span className='text-[18px] mb-4 mt-4'>Choose the event cover</span>
-          <input 
-            type="file" 
-            accept="image/*"
-            onChange={convertToBase64}
-          />
-          {
-              thumbnail == "" || thumbnail == null ? <Image width={144} height={144} src={PlusSign} style={{borderRadius:'8px'}} alt="" /> :
-              <Image width={144} height={144} src={thumbnail} style={{borderRadius:'8px'}} alt="" />
-          }
-          <span className='text-[18px] mb-4 mt-4'>Choose the event date</span>
-
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[280px] justify-start text-left font-normal",
-                  !date && "text-muted-foreground"
-                )}
-                onChange={(e) => setEventDate(e.currentTarget.value)}
-                aria-required="true"
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {date ? format(date, "PPP") : <span>Pick a date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={date}
-                onSelect={setDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-
-          <span className='text-[18px] mb-4 mt-4'>Choose the event title</span>
-          <input 
-            type="text" 
-            placeholder='&nbsp; Event title' 
-            className='h-10' 
-            value={title}
-            onChange={(e) => setTitle(e.currentTarget.value)}
-          />
-          <span className='text-[18px] mb-4 mt-4'>Choose the event type</span>
-          
-          {/* <TypeInput /> */}
-
-          <EventOptionType />
-
-          <span className='text-[18px] mb-4 mt-4'>Choose the event Platform</span>
-          {/* <PlatformInput /> */}
-
-          <EventPlatformOption />
-
-          <span className='text-[18px] mb-4 mt-4'>Event Description</span>
-
-          <div className="grid w-full gap-1.5 mb-4 bg-[#3B3B3B]">
-            <Textarea  placeholder="Type the event description here." value={description} onChange={(e) => setDescription(e.currentTarget.value)} />
-          </div>
-
-          <span className='text-[18px] mb-4 mt-4'>Event Link / Discord Link</span>
-          
-          <div className="grid w-full gap-1.5 mb-4 bg-[#3B3B3B]">
-            <input 
-              type="url" 
-              placeholder='&nbsp; Event link' 
-              className='h-10' 
-              value={eventLink}
-              onChange={(e) => setEventLink(e.currentTarget.value)}
-            />
-          </div>
-      
-          <button className='h-[51px] w-[146px] bg-[#FFCD00] text-[black] font-[500]' onClick={uploadEvent}>POST EVENT</button>
-        </section>
-      </div>
+    <div className=" bg-[#AA00FF] w-full flex h-screen">
+      <section className="">
+        <h1 className="text-xl font-medium text-center">Create your event</h1>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <Label htmlFor="email">Title</Label>
+          <Input type="email" id="email" placeholder="Tittle of the event" />
+          <Label htmlFor="picture">Picture</Label>
+          <Input id="picture" type="file" />
+          <Label htmlFor="date">Pick a date</Label>
+          <DatePicker />
+          <Label htmlFor="type">Type</Label>
+          <TypeInput />
+          <Label htmlFor="platform">Platform</Label>
+          <PlatformInput />
+          <Button className="h-12 w-[146px] bg-[#FFCD00] text-[black] font-[500]">
+            Post event
+          </Button>
+        </div>
+      </section>
     </div>
   )
 }
-
-          {/* <Select>
-            <SelectTrigger className="w-[180px] h-[51px] text-[black]">
-              <span className="-mr-8"><MapPin /></span>
-              <SelectValue placeholder="Platform" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup onChange={(e) => setSelectedPlatformOption(e.currentTarget.value)}>
-                <SelectLabel>Platforms</SelectLabel>
-                  {platformOptions.map((o) => (
-                    <SelectItem key={o.value} value={o.value}>{o.label}</SelectItem>
-                  ))}
-              </SelectGroup>
-            </SelectContent>
-          </Select> */}
-
-                    {/* <Select>
-            <SelectTrigger className="w-[180px] h-[51px] text-[black]">
-              <span><Grid /></span>
-              <SelectValue placeholder="Type of event" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Events</SelectLabel>
-                    <SelectItem value="Any">Any</SelectItem>
-                    <SelectItem value="+18">+18</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select> */}
-
-                    {/* <Select>
-            <SelectTrigger className="w-[180px] h-[51px] text-[black]">
-              <span><Grid /></span>
-              <SelectValue placeholder="Type of event" />
-            </SelectTrigger>
-            <SelectContent >
-              <SelectGroup>
-                <SelectLabel>Events</SelectLabel>
-                {typeOptions.map((o) => (
-                    <SelectItem key={o.value} value={o.value}>{o.label}</SelectItem>
-                  ))} 
-              </SelectGroup>
-            </SelectContent>
-          </Select> */}
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,39 +4,39 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 
-const homeURL = () => window.location.href;
+const homeURL = () => window.location.href
 export function SiteHeader() {
   return (
     <header className="fixed z-40 w-full ">
       <div className="flex items-center h-16 sm:justify-between sm:space-x-0">
         <div className="absolute flex gap-6 left-8 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="inline-block font-[300] text-[#FFCD00]">
+            <span className="inline-block font-semibold text-[#FFCD00]">
               V-REJECTS
             </span>
           </Link>
         </div>
         <div className="flex items-center justify-end flex-1 pr-8 space-x-4">
           <nav className="flex items-center space-x-1">
-
-                <Link
-                href={siteConfig.links.github}
-                target="_blank"
-                rel="noreferrer"
+            {/* <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                })}
               >
-                <div
-                  className={buttonVariants({
-                    size: "sm",
-                    variant: "ghost",
-                  })}
-                >
-                  <Icons.discord className="h-5 w-5 text-[#FFCD00]" />
-                  <span className="sr-only">Discord</span>
-                </div>
-              </Link>
+                <Icons.discord className="h-5 w-5 text-[#FFCD00]" />
+                <span className="sr-only">Discord</span>
+              </div>
+            </Link> */}
 
-              <Link href="/postevent" className="text-[#FFCD00] font-[500]">POST EVENT</Link>
-
+            <Link href="/postevent" className="text-[#FFCD00] font-semibold">
+              POST EVENT
+            </Link>
           </nav>
         </div>
       </div>
