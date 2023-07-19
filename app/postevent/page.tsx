cacc6bbce3ff957f45faad829cb6d80ac262826c
Leo <leonardomjq@gmail.com>
2023-07-19 12:30:47 +0100

    multiple new inputs

--- app/postevent/page.tsx
+++ app/postevent/page.tsx
@@ -2,13 +2,20 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { format } from "date-fns"
-import { Calendar as CalendarIcon } from "lucide-react"
+import { Calendar as CalendarIcon, Check, ChevronsUpDown } from "lucide-react"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
 import {
   Form,
   FormControl,
@@ -18,22 +25,65 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
 import { toast } from "@/components/ui/use-toast"
 import { SiteHeader } from "@/components/site-header"
 
+const timezone = [
+  { label: "Pacific Standard Time (PST)", value: "GMT-08:00" },
+  { label: "Mountain Standard Time (MST)", value: "GMT-07:00" },
+  { label: "Central Standard Time (CST)", value: "GMT-06:00" },
+  { label: "Eastern Standard Time (EST)", value: "GMT-05:00" },
+  { label: "Greenwich Mean Time (GMT)", value: "GMT+00:00" },
+  { label: "Coordinated Universal Time (UTC)", value: "GMT+00:00" },
+  { label: "Central European Time (CET)", value: "GMT+01:00" },
+  { label: "Eastern European Time (EET)", value: "GMT+02:00" },
+  { label: "Indian Standard Time (IST)", value: "GMT+05:30" },
+  { label: "China Standard Time (CST)", value: "GMT+08:00" },
+  { label: "Japan Standard Time (JST)", value: "GMT+09:00" },
+  { label: "Australian Eastern Standard Time (AEST)", value: "GMT+10:00" },
+] as const
+
 const FormSchema = z.object({
+  title: z.string({
+    required_error: "A title is required.",
+  }),
+  description: z
+    .string()
+    .min(24, {
+      message: "Description must be at least 24 characters.",
+    })
+    .max(420, {
+      message: "Description must not be longer than 420 characters.",
+    }),
   dob: z.date({
     required_error: "A date of birth is required.",
+  }),
+  email: z
+    .string({
+      required_error: "Please select an email to display.",
+    })
+    .email(),
+  timezone: z.string({
+    required_error: "Please select a language.",
   }),
 })
 
 export default function page() {
-  
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   })
@@ -57,17 +107,53 @@ export default function page() {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <FormField
               control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Title of the event" {...field} />
+                  </FormControl>
+                  <FormDescription>Keep it simple.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Tell us a little bit about the event"
+                      className="resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Be creative and keep it simple.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
               name="dob"
               render={({ field }) => (
                 <FormItem className="flex flex-col">
-                  <FormLabel>Date of birth</FormLabel>
+                  <FormLabel>Date of the event</FormLabel>
                   <Popover>
                     <PopoverTrigger asChild>
                       <FormControl>
                         <Button
                           variant={"outline"}
                           className={cn(
-                            "w-[240px] pl-3 text-left font-normal",
+                            "w-full pl-3 text-left font-normal",
                             !field.value && "text-muted-foreground"
                           )}
                         >
@@ -99,6 +185,102 @@ export default function page() {
                 </FormItem>
               )}
             />
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="m@example.com">
+                        m@example.com
+                      </SelectItem>
+                      <SelectItem value="m@google.com">m@google.com</SelectItem>
+                      <SelectItem value="m@support.com">
+                        m@support.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormDescription>
+                    You can manage email addresses in your
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="timezone"
+              render={({ field }) => (
+                <FormItem className="flex flex-col">
+                  <FormLabel>Timezone</FormLabel>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <FormControl>
+                        <Button
+                          variant="outline"
+                          role="combobox"
+                          className={cn(
+                            "w-[200px] justify-between",
+                            !field.value && "text-muted-foreground"
+                          )}
+                        >
+                          {field.value
+                            ? timezone.find(
+                                (timezone) => timezone.value === field.value
+                              )?.label
+                            : "Select timezone"}
+                          <ChevronsUpDown className="w-4 h-4 ml-2 opacity-50 shrink-0" />
+                        </Button>
+                      </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                      <Command>
+                        <CommandInput placeholder="Search framework..." />
+                        <CommandEmpty>No framework found.</CommandEmpty>
+                        <CommandGroup>
+                          {timezone.map((timezone) => (
+                            <CommandItem
+                              value={timezone.value}
+                              key={timezone.value}
+                              onSelect={(value) => {
+                                form.setValue("timezone", value)
+                              }}
+                            >
+                              <Check
+                                className={cn(
+                                  "mr-2 h-4 w-4",
+                                  timezone.value === field.value
+                                    ? "opacity-100"
+                                    : "opacity-0"
+                                )}
+                              />
+                              {timezone.label}
+                            </CommandItem>
+                          ))}
+                        </CommandGroup>
+                      </Command>
+                    </PopoverContent>
+                  </Popover>
+                  <FormDescription>
+                    This is the language that will be used in the dashboard.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
             <Button type="submit">Submit</Button>
           </form>
         </Form>
